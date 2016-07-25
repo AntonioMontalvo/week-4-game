6e92e5a7052535fb824fe5ab0a2d431c3a84eb29
Antonio Montalvo <montalvocode@yahoo.com>
2016-07-25 15:07:52 -0700

    add test

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,6 +4,9 @@
 var score = 0;
 var guess;
 var crystalValues = [];
+var wins = 0;
+var losses = 0;
+
 
 
 function randomNumber(lowerNumber, upperNumber){
@@ -14,14 +17,17 @@ function adder(name){ //add selected image value to score
   score = score + ($(name).data('num'));
   $('#userScore').text(score);
 }
+function test(){
+  if ( score === parseInt(guess) ){
+    console.log('matched');
+  }
+}
 
 
 ////////////////////////////////////////////////////
 //                  loaded document               //
 ////////////////////////////////////////////////////
 $('document').ready(function(){ //the DOM is now loaded and can be manipulated
-  randomNumber(19, 120); //call
-  $('#guessNumber').text(guess);
 
   var crystalImages = [$('#diamond'),$('#opal'),$('#diamond2'),$('#ruby')];//index each crystal image
   for (var i = 0; i < crystalImages.length; i++) {
@@ -33,15 +39,22 @@ $('document').ready(function(){ //the DOM is now loaded and can be manipulated
 /////////handler/////////////////////////
   $('#diamond').on('click', function(){
     adder('#diamond');
+    test();
   });
   $('#opal').on('click', function(){
     adder('#opal');
+    test();
   });
   $('#diamond2').on('click', function(){
     adder('#diamond2');
+    test();
   });
   $('#ruby').on('click', function(){
     adder('#ruby');
+    test();
   });
+  randomNumber(19, 120); //call
+
+  $('#guessNumber').text(guess);
 
 });
