8e2a8ff814b52297c6043a40424689a22b88ec0d
Antonio Montalvo <montalvocode@yahoo.com>
2016-07-25 10:23:03 -0700

    add adder function, assigh numbers to images, onclick handlers and adder function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,24 +5,43 @@ var score = 0;
 var guess;
 var crystalValues = [];
 
+
 function randomNumber(lowerNumber, upperNumber){
   guess = Math.floor(Math.random()*(upperNumber - lowerNumber + 1) + lowerNumber);
   return guess;
 }
+function adder(name){ //add selected image value to score
+  score = score + ($(name).data('num'));
+  $('#userScore').text(score);
+}
+
 
 ////////////////////////////////////////////////////
-//                    loaded                      //
+//                  loaded document               //
 ////////////////////////////////////////////////////
 $('document').ready(function(){ //the DOM is now loaded and can be manipulated
   randomNumber(19, 120); //call
-
-  $('#userScore').text(score);
-
   $('#guessNumber').text(guess);
 
-  for (var i = 0; i < 4; i++) {
+  var crystalImages = [$('#diamond'),$('#opal'),$('#diamond2'),$('#ruby')];//index each crystal image
+  for (var i = 0; i < crystalImages.length; i++) {
     randomNumber(1, 12);
-    crystalValues.push(guess);
+    crystalValues.push(guess);//generate 4  between 1 & 12 numbers and push to array
+    crystalImages[i].attr('data-num', crystalValues[i]);// assign each number to each image.
   }
+
+/////////handler/////////////////////////
+  $('#diamond').on('click', function(){
+    adder('#diamond');
+  });
+  $('#opal').on('click', function(){
+    adder('#opal');
+  });
+  $('#diamond2').on('click', function(){
+    adder('#diamond2');
+  });
+  $('#ruby').on('click', function(){
+    adder('#ruby');
+  });
 
 });
