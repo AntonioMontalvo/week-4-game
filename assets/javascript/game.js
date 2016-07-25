04ba5f259bf483c71fd723bd9bd9ed241dbd73f4
Antonio Montalvo <montalvocode@yahoo.com>
2016-07-24 22:37:13 -0700

    add id to each crystal/ loop generates 4 values and stores on crystalValues array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,7 @@
 ////////////////////////////////////////////////////
 var score = 0;
 var guess;
+var crystalValues = [];
 
 function randomNumber(lowerNumber, upperNumber){
   guess = Math.floor(Math.random()*(upperNumber - lowerNumber + 1) + lowerNumber);
@@ -10,19 +11,18 @@ function randomNumber(lowerNumber, upperNumber){
 }
 
 ////////////////////////////////////////////////////
-//                    GLOBAL                      //
+//                    loaded                      //
 ////////////////////////////////////////////////////
-$('document').ready(function(){
+$('document').ready(function(){ //the DOM is now loaded and can be manipulated
   randomNumber(19, 120); //call
 
   $('#userScore').text(score);
 
   $('#guessNumber').text(guess);
 
-
-
-
-
-
+  for (var i = 0; i < 4; i++) {
+    randomNumber(1, 12);
+    crystalValues.push(guess);
+  }
 
 });
