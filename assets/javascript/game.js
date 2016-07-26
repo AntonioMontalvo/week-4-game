c1f39c91dace7af704effbdd7ff5db31c9553070
Antonio Montalvo <montalvocode@yahoo.com>
2016-07-25 19:13:57 -0700

    final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,59 +2,80 @@
 //                    GLOBAL                      //
 ////////////////////////////////////////////////////
 var score = 0;
-var guess;
+var guess = 0;
 var crystalValues = [];
+var crystalImages = [$('#diamond'),$('#opal'),$('#diamond2'),$('#ruby')];//index each crystal image
 var wins = 0;
 var losses = 0;
 
-
+$('#wins').text(wins);
+$('#losses').text(losses);
+var youWonorLost = $('<div id="game-end"></div>');
+$('#wonlost').append(youWonorLost)
 
 function randomNumber(lowerNumber, upperNumber){
   guess = Math.floor(Math.random()*(upperNumber - lowerNumber + 1) + lowerNumber);
   return guess;
 }
 function adder(name){ //add selected image value to score
-  score = score + ($(name).data('num'));
+  score = score + name;
   $('#userScore').text(score);
+}
+function newCrystalValues(){
+  for (var i = 0; i < crystalImages.length; i++) {
+    randomNumber(1, 12);
+    crystalValues.push(guess);//generate 4  between 1 & 12 numbers and push to array
+  }
 }
 function test(){
   if ( score === parseInt(guess) ){
-    console.log('matched');
+    wins++;
+    $('#wins').text(wins);
+    score = 0;
+    $('#userScore').text(score);
+    newCrystalValues(); //call new crystal values
+    randomNumber(19, 120); //call new number to match
+    $('#guessNumber').text(guess);
+    $('#game-end').text('YOU WON');
+  }
+  if ( score > parseInt(guess) ){
+    losses++;
+    $('#losses').text(losses);
+    score = 0;
+    $('#userScore').text(score);
+    crystalValues = []; //empty the array
+    newCrystalValues(); //call new crystal values
+    randomNumber(19, 120); //call new number to match
+    $('#guessNumber').text(guess);
+    $('#game-end').text('YOU LOST');
   }
 }
-
 
 ////////////////////////////////////////////////////
 //                  loaded document               //
 ////////////////////////////////////////////////////
 $('document').ready(function(){ //the DOM is now loaded and can be manipulated
-
-  var crystalImages = [$('#diamond'),$('#opal'),$('#diamond2'),$('#ruby')];//index each crystal image
-  for (var i = 0; i < crystalImages.length; i++) {
-    randomNumber(1, 12);
-    crystalValues.push(guess);//generate 4  between 1 & 12 numbers and push to array
-    crystalImages[i].attr('data-num', crystalValues[i]);// assign each number to each image.
-  }
+  newCrystalValues();
+  randomNumber(19, 120); //call
+  $('#guessNumber').text(guess);
 
 /////////handler/////////////////////////
   $('#diamond').on('click', function(){
-    adder('#diamond');
+    adder(crystalValues[0]);
     test();
   });
   $('#opal').on('click', function(){
-    adder('#opal');
+    adder(crystalValues[1]);
     test();
   });
   $('#diamond2').on('click', function(){
-    adder('#diamond2');
+    adder(crystalValues[2]);
     test();
   });
   $('#ruby').on('click', function(){
-    adder('#ruby');
+    adder(crystalValues[3]);
     test();
   });
-  randomNumber(19, 120); //call
 
-  $('#guessNumber').text(guess);
 
 });
